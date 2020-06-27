0ad4082ad64de8c309bfb03207516ce85aa6e553
ChanYiHong <chanyinara@naver.com>
2020-06-27 15:38:24 +0900

    Item Entity Business logic

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -31,4 +32,30 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+
+    // ==비즈니스 로직==//
+    // 객체지향적. stockQuantity가 이 entity 안에 있음
+    // 이 데이터를 가지고 있는 쪽에서 비즈니스 로직이 나가는게 응집도가 있음
+
+    /**
+     *
+     * stock 증가.
+     */
+
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     *
+     * stock 감소.
+     */
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
