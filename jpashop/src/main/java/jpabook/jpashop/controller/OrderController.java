c9b861b9cfb0b35eb6323e778978355efb3cd3dc
ChanYiHong <chanyinara@naver.com>
2020-07-01 23:57:50 +0900

    Item Order Controller

---
+++ jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    // 고객이랑 상품이랑 다 선택해야 함
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model){
+
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+
+    @PostMapping("/order")
+    // form submit방식에서 넘겨지는, html의 name 값으로 넘어옴. 변수에 바인딩 시킴
+    public String order(@RequestParam("memberId") Long memberId
+                        ,@RequestParam("itemId") Long itemId
+                        ,@RequestParam("count") int count){
+
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
