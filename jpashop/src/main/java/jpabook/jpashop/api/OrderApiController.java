f704e261b7f0d4a708c81c83fa52c12469ac088c
ChanYiHong <chanyinara@naver.com>
2020-11-19 23:26:02 +0900

    Collection fetch join + paging batch_size study

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,10 +6,13 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -21,6 +24,7 @@ import java.util.stream.Collectors;
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1(){
@@ -45,6 +49,40 @@ public class OrderApiController {
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
         return new Result(result);
+    }
+
+
+    @GetMapping("/api/v3/orders")
+    public Result ordersV3(){
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return new Result(result);
+    }
+
+
+    @GetMapping("/api/v3.1/orders")
+    public Result ordersV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit
+    ){
+
+        List<Order> toOne = orderRepository.findAllWithMemberDelivery(offset, limit);
+
+        List<OrderDto> result1 = toOne.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return new Result(result1);
+
+    }
+
+
+    @GetMapping("/api/v4/orders")
+    public Result ordersV4(){
+        List<OrderQueryDto> orders = orderQueryRepository.findOrderQueryDtos();
+        return new Result(orders);
     }
 
     @Data
---
+++ jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.repository.order.query;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class OrderQueryRepository {
+
+    private final EntityManager em;
+
+    public List<OrderQueryDto> findOrderQueryDtos() {
+        List<OrderQueryDto> result = findOrders();
+        result.stream()
+                .forEach(o -> {
+                    List<OrderItemQueryDto> orderItems = findOrderItems(o.getOrderId());
+                    o.setOrderItems(orderItems);
+                });
+        return result;
+    }
+
+    // orderId를 통해 따로 뽑음.
+    private List<OrderItemQueryDto> findOrderItems(Long orderId) {
+        return em.createQuery("select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                " from OrderItem oi" +
+                " join oi.item i" +
+                " where oi.order.id = :orderId", OrderItemQueryDto.class)
+                .setParameter("orderId", orderId)
+                .getResultList();
+    }
+
+
+    // 컬렉션은 new 연산 조회 불가능. 따로 뽑아야 됨.
+    private List<OrderQueryDto> findOrders() {
+        return em.createQuery(
+                "select new jpabook.jpashop.repository.order.query.OrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address) from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d", OrderQueryDto.class)
+                .getResultList();
+    }
+}
