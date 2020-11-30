2acc71b0acd9e75da8932023aebda688b80bfae1
ChanYiHong <chanyinara@naver.com>
2020-11-30 23:59:36 +0900

    EntityGraph

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -247,4 +247,40 @@ class MemberRepositoryTest {
         // then
         assertThat(resultCount).isEqualTo(4);
     }
+
+    @Test
+    public void findMemberLazy(){
+        //given
+        //member1 -> teamA
+        //member2 -> teamB
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        //when
+
+        List<Member> members = memberRepository.findAll();
+
+        for (Member member : members) {
+            System.out.println(member.getUsername());
+            System.out.println(member.getTeam().getName());
+        }
+
+        List<Member> memberFetches = memberRepository.findMemberFetchJoin();
+
+        for (Member member : memberFetches) {
+            System.out.println(member.getUsername());
+            System.out.println(member.getTeam().getName());
+        }
+
+    }
 }
