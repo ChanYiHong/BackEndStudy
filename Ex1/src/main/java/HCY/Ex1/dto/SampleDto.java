0406865b51b5b0a673b42c65b11d3830b9c1c51f
ChanYiHong <chanyinara@naver.com>
2021-01-08 20:18:29 +0900

    Thymeleaf Practice

---
+++ Ex1/src/main/java/HCY/Ex1/dto/SampleDto.java
@@ -0,0 +1,17 @@
+package HCY.Ex1.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDto {
+
+    private Long sno;
+    private String first;
+    private String last;
+    private LocalDateTime regTime;
+
+}
