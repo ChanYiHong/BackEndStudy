dba4830e069d9d81a1b2647e8891bfacea300d65
ChanYiHong <chanyinara@naver.com>
2021-11-19 23:53:53 +0900

    Interface 기반 Proxy

---
+++ proxy/src/main/java/hello/proxy/config/v1_proxy/InterfaceProxyConfig.java
@@ -0,0 +1,34 @@
+package hello.proxy.config.v1_proxy;
+
+import hello.proxy.app.v1.*;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderControllerInterfaceProxy;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderRepositoryInterfaceProxy;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderServiceInterfaceProxy;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/** 애플리케이션 실행 시점에 프록시를 사용하도록 의존 관계를 설정해주어야 한다.
+ * 프록시를 실제 스프링 빈 대신 등록. 실제 객체는 스프링 빈으로 등록 x. **/
+@Configuration
+public class InterfaceProxyConfig {
+
+    @Bean
+    public OrderControllerV1 orderController(LogTrace logTrace) {
+        OrderControllerV1Impl controllerImpl = new OrderControllerV1Impl(orderService(logTrace));
+        return new OrderControllerInterfaceProxy(controllerImpl, logTrace);
+    }
+
+    @Bean
+    public OrderServiceV1 orderService(LogTrace logTrace) {
+        OrderServiceV1Impl serviceImpl = new OrderServiceV1Impl(orderRepository(logTrace));
+        return new OrderServiceInterfaceProxy(serviceImpl, logTrace);
+    }
+
+    @Bean
+    public OrderRepositoryV1 orderRepository(LogTrace logTrace) {
+        OrderRepositoryV1Impl repositoryImpl = new OrderRepositoryV1Impl();
+        return new OrderRepositoryInterfaceProxy(repositoryImpl, logTrace);
+    }
+
+}
