5ab6eedf63fb6e33b4cfe19510c7d408f6e72411
ChanYiHong <chanyinara@naver.com>
2021-11-22 17:04:37 +0900

    BeanPostProcessor 빈 후처리기 사용해보기.

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -8,8 +8,10 @@ import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +21,8 @@ import org.springframework.context.annotation.Import;
 //@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
 //@Import({DynamicProxyBasicConfig.class})
 //@Import({DynamicProxyFilterConfig.class})
-@Import({ProxyFactoryConfigV1.class, ProxyFactoryConfigV2.class})
+//@Import({ProxyFactoryConfigV1.class, ProxyFactoryConfigV2.class})
+@Import(BeanPostProcessorConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") // hello.proxy.app 하위만 컴포넌트 스캔.
 public class ProxyApplication {
 
