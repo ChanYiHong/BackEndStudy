0e7c3c8bd0af37dac05765552711d2bb780e1a25
ChanYiHong <chanyinara@naver.com>
2021-11-22 14:00:11 +0900

    ProxyFactory 적용해보기.

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -6,6 +6,8 @@ import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -16,7 +18,8 @@ import org.springframework.context.annotation.Import;
 //@Import({AppV1Config.class, AppV2Config.class}) // 컴포넌트 스캔 대상으로 만들기 위해 Import
 //@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
 //@Import({DynamicProxyBasicConfig.class})
-@Import({DynamicProxyFilterConfig.class})
+//@Import({DynamicProxyFilterConfig.class})
+@Import({ProxyFactoryConfigV1.class, ProxyFactoryConfigV2.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") // hello.proxy.app 하위만 컴포넌트 스캔.
 public class ProxyApplication {
 
