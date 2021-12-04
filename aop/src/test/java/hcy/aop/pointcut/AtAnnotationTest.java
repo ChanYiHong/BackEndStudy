b6d542fa777e51172b3f907485c63cbfaad98c96
ChanYiHong <chanyinara@naver.com>
2021-12-04 22:29:09 +0900

    @Annotation, bean 포인트컷 지시자.

---
+++ aop/src/test/java/hcy/aop/pointcut/AtAnnotationTest.java
@@ -0,0 +1,40 @@
+package hcy.aop.pointcut;
+
+import hcy.aop.member.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(AtAnnotationTest.AtAnnotationAspect.class)
+@SpringBootTest
+public class AtAnnotationTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    void success() {
+        log.info("memberService Proxy={}", memberService.getClass());
+        memberService.hello("helloA");
+    }
+
+    @Slf4j
+    @Aspect
+    static class AtAnnotationAspect {
+
+        // @MethodAop 가 적용된 메서드에 어드바이스가 적용됨.
+        @Around("@annotation(hcy.aop.member.annotation.MethodAop)")
+        public Object doAtAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[@annotation] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+    }
+
+}
